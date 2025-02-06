d766784ac0cc32ae97218d7e194372b218b312ad
Dao Chau Nghia <daochaunghia@gmail.com>
2025-02-06 23:46:35 +0700

    upload the first time

---
+++ Core/Inc/Init/timers.h
@@ -0,0 +1,15 @@
+/*
+ * timers.h
+ *
+ *  Created on: Feb 6, 2025
+ *      Author: daoch
+ */
+
+#ifndef INC_INIT_TIMERS_H_
+#define INC_INIT_TIMERS_H_
+
+void timersInit(void);
+void timerPinSetup(void);
+void timersRegisterSetup(void);
+
+#endif /* INC_INIT_TIMERS_H_ */
---
+++ Core/Src/Threads/uart_control.c
@@ -0,0 +1,36 @@
+/*
+ * uart_control.c
+ *
+ *  Created on: Feb 6, 2025
+ *      Author: daoch
+ */
+
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <cmsis_os2.h>
+#include "uart_control.h"
+#include "uart.h"
+
+
+void CreateUARTLedControlThread(void) {
+  // uartLedControl Thread
+  osThreadId_t uartLedControl;
+  const osThreadAttr_t uartLedControl_attributes ={
+	.name = "uartLedControl",
+	.stack_size = 512 * 4,
+	.priority = (osPriority_t) osPriorityNormal,
+  };
+
+  uartLedControl = osThreadNew(startUARTLedControl, NULL, &uartLedControl_attributes);
+  if (NULL == uartLedControl) PRINT_UART("uartLedControl Thread is failed to be created\r\n");
+  else PRINT_UART("uartLedControl Thread created successfully\r\n");
+}
+void startUARTLedControl(void *argument) {
+	PRINT_UART("uartLedControl Thread run \r\n");
+	osDelay(1);
+	for (;;) {
+		osDelay(1);
+	}
+}
